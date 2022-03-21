c0a64ed016dd461f678bb03047216100816681a9
bettyzhuangg <87810403+bettyzhuangg@users.noreply.github.com>
2022-03-21 21:00:20 +1100

    Create buddysystem2.ino

---
+++ Task3.3D-CloudFunction/buddysystem2.ino
@@ -0,0 +1,39 @@
+// pin where the LED is connected to
+const pin_t LED = D3;
+// RIOT of photon buddy that detects waving motion
+const char *Event = "Deakin_RIOT_SIT210_Photon_Buddy";
+
+void setup()
+{
+    // declare the pin as output pin
+	pinMode(LED, OUTPUT);
+    // subscribe to the RIOT
+	Particle.subscribe(Event, handler);
+}
+
+
+void handler(const char *event, const char *data)
+{
+    // if waving motion is detected
+    if (strcmp(data,"wave") == 0)
+    {
+        blink(3,500);
+    }
+    // if patting motion is detected
+    if (strcmp(data,"pat") == 0)
+    {
+        blink(5,500);
+    }
+}
+
+// function for blinking the LED
+void blink(int blinks, int interval)
+{
+    for(int i = 0; i < blinks; i++)
+    {
+        digitalWrite(LED, HIGH);
+        delay(interval);
+        digitalWrite(LED, LOW);
+        delay(interval);
+    }
+}
